61fe077c04ecbe5a649b0ed9c5e149c3cf653332
ZainKizilbash <56188371+ZainKizilbash@users.noreply.github.com>
2019-10-16 00:23:45 +0500

    Removes a selected character

---
+++ Remove Character.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim sen, charac, nextchar, str2 As String
+        Dim counter As Integer
+        sen = " "
+        charac = " "
+        nextchar = " "
+        str2 = " "
+        counter = 0
+        Console.Write("Enter Sentence: ")
+        sen = Console.ReadLine
+        Console.Write("Character to remove: ")
+        charac = Console.ReadLine
+        For counter = 1 To Len(sen)
+            nextchar = Mid(sen, counter, 1)
+            If nextchar <> charac Then
+                str2 = str2 & nextchar
+
+            End If
+        Next
+        Console.Write("Final Name: " & str2)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
